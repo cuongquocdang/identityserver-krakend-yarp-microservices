8b9b45b5fa26f1e2765637ce253791d666678883
Cuong Quoc Dang <cuongdang.developer@gmail.com>
2022-09-18 21:36:14 +0700

    feat(resource-server): add security configuration

--- SampleService-ResourceServer/sample-service/src/main/java/com/example/sampleservice/controllers/SampleController.java
+++ SampleService-ResourceServer/sample-service/src/main/java/com/example/sampleservice/controllers/SampleController.java
@@ -1,5 +1,7 @@
 package com.example.sampleservice.controllers;
 
+import com.example.jwtsecurityautoconfigure.JWTSecurityUtil;
+import com.example.jwtsecurityautoconfigure.JWTUserInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,11 @@ public class SampleController {
     @GetMapping("/test")
     public String test() {
         return "Test!";
+    }
+
+    @GetMapping("/user-info")
+    public JWTUserInfo retrieveUserInfo() {
+        return JWTSecurityUtil.getJWTUserInfo().orElse(null);
     }
 
 }
